5dfd7364ebc8bdbf2e3ea5b4b42303e6018a862a
n00r <noureddine.bouzidi@gmail.com>
2025-03-23 03:27:28 +0100

    A gcc pkg

--- hosts/n00r-laptop/configuration.nix
+++ hosts/n00r-laptop/configuration.nix
@@ -1,15 +1,16 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -83,6 +84,7 @@
     wget
     neovim
     git
+    gcc
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -135,6 +137,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
-
