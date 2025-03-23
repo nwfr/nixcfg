3896179b362aa71672b16b31c7077895508b9b93
n00r <noureddine.bouzidi@gmail.com>
2025-03-23 05:24:51 +0100

    refctor dotfiles

--- home/n00r/dotfiles/default.nix
+++ home/n00r/dotfiles/default.nix
@@ -1,10 +1,30 @@
-{inputs, ...}: {
-  imports = [
-    ./bat.nix
-  ];
-
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   home.file.".config/nvim" = {
     source = "${inputs.dotfiles}/nvim";
     recursive = true;
   };
+
+  home.file.".config/bat" = {
+    source = "${inputs.dotfiles}/bat";
+    recursive = true;
+  };
+
+  # home.file.".config/nyxt" = {
+  #   source = "${inputs.dotfiles}/nyxt";
+  #   recursive = true;
+  # };
+
+  # home.file.".config/zellij" = {
+  #   source = "${inputs.dotfiles}/zellij";
+  # recursive = true;
+  # };
+
+  # home.file.".config/hypr" = {
+  #   source = "${inputs.dotfiles}/hypr";
+  #   recursive = true;
+  # };
 }
